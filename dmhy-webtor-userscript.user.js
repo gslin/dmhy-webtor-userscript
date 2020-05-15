f746f6dce49a7bee4fe7d6b56ff00770034de2ce
Gea-Suan Lin <gslin@gslin.org>
2020-05-15 22:55:08 +0800

    Keep some tr information.

--- dmhy-webtor-userscript.user.js
+++ dmhy-webtor-userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMHY with WebTor.io
 // @namespace    https://github.com/gslin/dmhy-webtor-userscript
-// @version      0.20200515.0
+// @version      0.20200515.1
 // @description  Add WebTor.io links to DMHY
 // @author       Gea-Suan Lin <gslin@gslin.com>
 // @match        https://share.dmhy.org/*
@@ -13,9 +13,8 @@
     'use strict';
 
     for (let arrow of document.querySelectorAll('a.download-arrow.arrow-magnet')) {
-        // magnet:?xt=urn:btih:TROPIGOFPTGGAL22NDEQVUUHHM7WEBKU
         let magneturi = arrow.getAttribute('href');
-        let magneturi_cleaned = magneturi.replace(/&.*/, '');
+        let magneturi_cleaned = magneturi.substring(0, 2048);
 
         let webtor_url = 'https://webtor.io/show?&pwd=%2F&magnet=' + encodeURIComponent(magneturi_cleaned) + '&mode=default'
 
